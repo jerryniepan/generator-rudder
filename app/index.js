aeb6533b4986bcc847b92544a49811d54ae7cbe9
xiefei <xiefei@haizhi.com>
2017-06-27 17:19:03 +0800

    rudder init

---
+++ app/index.js
@@ -0,0 +1,33 @@
+var Generator = require('yeoman-generator');
+
+module.exports = Generator.extend({
+    prompts: function() {
+        return this.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'What is your project name?',
+            defualt: this.appname
+        }, {
+            type: 'input',
+            name: 'description',
+            message: 'What is your project\'s description?'
+        }]).then((answers) => {
+            this.name = answers.name;
+            this.description = answers.description;
+        });
+    },
+    initialCopy: function() {
+        this.fs.copy(
+            this.templatePath(''),
+            this.destinationPath(this.name)
+        );
+    },
+    end: function() {
+        var npmDir = process.cwd() + '/' + this.name;
+        process.chdir(npmDir);
+        this.installDependencies({
+            bower: false,
+            npm: true
+        });
+    }
+});
